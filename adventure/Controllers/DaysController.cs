c90d922161aeb32f3de918ac11816a9adbc6a952
Simon Harriyott <simon@harriyott.com>
2015-10-03 00:22:21 +0100

    Day controller returns minimal JSON
    /api/days

---
+++ adventure/Controllers/DaysController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Adventure.Controllers
+{
+    public class DaysController : ApiController
+    {
+        // GET api/days
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new
+            {
+                Days = new[] {
+                            new {Day = 1 },
+                            new {Day = 2}
+                   }
+            });
+        }
+    }
+}
