5b6f4bdfd328d5bf43321ddb31f2379df9e5d81f
Simon Harriyott <simon@harriyott.com>
2015-10-03 12:29:31 +0100

    Removed loop exit code

--- adventure/Services/TwitterHashtagMonitor.cs
+++ adventure/Services/TwitterHashtagMonitor.cs
@@ -34,7 +34,7 @@ namespace Adventure.Services
         public async Task Monitor()
         {
             var search = string.Join(",", Enumerable.Range(1, 24).Select(d => "AdventHunt" + d));
-            var count = 0;
+
             var twitterCtx = new TwitterContext(Authorizer);
             await
                 (from strm in twitterCtx.Streaming
