5979651aaa6db6a3244632d13025c89d637a4699
Simon Harriyott <simon@harriyott.com>
2015-10-03 01:51:14 +0100

    Added day class and entity framework

---
+++ adventure/Models/Day.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Adventure.Models
+{
+    [Table("Day")]
+    public class Day
+    {
+        public virtual int DayNumber { get; set; }
+    }
+}
