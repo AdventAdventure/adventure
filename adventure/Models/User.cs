1fb8a92763582e1f1c9e606a3368fe51ca5b3d55
Arran France <arranf@users.noreply.github.com>
2015-10-03 03:00:05 +0100

    Add User Model

---
+++ adventure/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Adventure.Models
+{
+    [Table("User")]
+    public class User
+    {
+        public virtual int UserId { get; set; }
+        public virtual string UserTwitterName { get; set; }
+    }
+}
