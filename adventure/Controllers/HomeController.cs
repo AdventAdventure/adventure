077cb9d0da293234690fff7ab9da6a56b545401d
Simon Harriyott <simon@harriyott.com>
2015-10-04 01:10:50 +0100

    Added default controller to return index.html

---
+++ adventure/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Adventure.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return File(Server.MapPath("~") + "index.html", "text/html");
+        }
+    }
+}
